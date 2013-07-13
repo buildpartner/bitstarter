23f0796d21b1083da3da7e2c075e0312beff6370
Marten Hauville <m.hauville@buildpartner.net>
2013-07-13 07:37:55 +0000

    HW3.1 showIndex First Test

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// HW3.1
+var index = fs.readFileSync('index.html'); // read "index.html" from disk
+buffer.write(index); // assign to buffer 
+var showIndex = buf.toString(); // read from buffer
+
+// original web.js function define showIndex
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(showIndex);
 });
 
 var port = process.env.PORT || 5000;
