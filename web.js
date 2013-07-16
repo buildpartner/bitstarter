1596b79fc735747236d22e78ae06f5af9a719616
Marten Hauville <m.hauville@buildpartner.net>
2013-07-16 08:21:24 +0000

    HW3.1 showIndex Fourth Test

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var app = express.createServer(express.logger());
 
 // HW3.1
 var fs = require('fs');
-var buf = new Buffer (fs.readFileSync('./index.html')); // read "index.html" from disk which should return buffer
+var buf = new Buffer (fs.readFileSync('index.html')); // read "index.html" from disk which should return buffer
 buffer.write(buf); // assign var buf to buffer 
-var showIndex = buf.toString(buf); // read from buffer
+var showIndex = buf.toString(); // read from buffer
 
 // original web.js function define showIndex
 app.get('/', function(request, response) {
