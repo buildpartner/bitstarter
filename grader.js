749f68034a5e4adb4870d5d5661b0074da5b2ece
Marten Hauville <m.hauville@buildpartner.net>
2013-07-19 05:03:40 +0000

    Version 2 - url entry edit

--- grader.js
+++ grader.js
@@ -27,6 +27,7 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+// input html file
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -35,6 +36,43 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+// input hardcode http
+rest.get('http://salty-earth-2427.herokuapp.com/').on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    sys.puts(result);
+  }
+});
+
+// above needs changing to work
+
+var callback = function(urlsourcecontent) {
+   var checkJson = checkHtmlFile(urlsourcecontent, program.checks);
+};
+
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <myUrl>','Path to external url')
+        .parse(process.argv);
+    if(program.url) {
+      rest.get(program.url).on('complete', callback);
+    }
+    else if (program.file) {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    else {
+      console.log("Please present a file or an url as parameter.");
+    }
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -64,6 +102,7 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url','Path to external url')
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
