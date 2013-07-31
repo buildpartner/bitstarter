4384d28613807ebd4bc931f4e154e9888ba5cf28
Marten Hauville <m.hauville@buildpartner.net>
2013-07-31 05:52:19 +0000

    Version 8 add express static

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ var showIndex = buf.toString(); // read from buffer
 app.get('/', function(request, response) {
   response.send(showIndex);
 });
-
+app.use(express.static(__dirname));
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
